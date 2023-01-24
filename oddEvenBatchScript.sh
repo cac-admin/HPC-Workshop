e9fc50323fdd471c2dbe1d47339ab3cedfd463ae
Shadi Khalifa <9610640+skhalifa@users.noreply.github.com>
2023-01-24 14:53:41 -0500

    Add files via upload

---
+++ oddEvenBatchScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#SBATCH --reservation=teaching
+#SBATCH --account=teaching
+#SBATCH --partition=standard
+#SBATCH --qos=normal
+#SBATCH --job-name=myJobName  #optional name to give to your job 
+#SBATCH -c 1 # Number of CPUS requested. If omitted, the default is 1 CPU. 
+#SBATCH --mem=4G # Memory requested in megabytes. If omitted, the default is 1024 MB. 
+#SBATCH --time=00:30:00 # Expected job run time (killed afterwards) default 3hrs. 
+
+python oddEvenSync.py
+
